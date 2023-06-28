f496dd9a60d67092bc48f41d1e3b98a2fd97720d
Matheus Araujo <mhma@ecomp.poli.br>
2023-06-27 23:52:51 -0300

    Adicionada multiplicacao e divisao

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
 fun main(){
-    
+
+}
+
+fun soma(){}
+fun sub(){}
+fun mult(n: Int, n2: Int): Int{
+    return n*n2
+}
+fun divs(n: Int, n2: Int): Int{
+    return if(n2 != 0){
+        n / n2
+    }else {
+        0
+    }
 }
